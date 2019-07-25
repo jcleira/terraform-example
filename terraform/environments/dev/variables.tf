3a8ae4b1e15c791d49f25893bde2c50bd2fd579b
jcleira <jmc.leira@gmail.com>
2019-07-25 08:23:24 +0200

    Add development environment for provisioning tests
    This commit adds an environment that would be used to test the
    provisioning for all the components that we've been configured.

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,8 @@
+/*
+* The name var flow to all components to create a homogeneus naming for the
+* provisioned resources
+*/
+variable "name" {
+  type = "string"
+  description = "The base name for all the resources to create"
+}
